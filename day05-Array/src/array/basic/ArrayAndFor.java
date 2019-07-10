41c1b48d3e452298d6d4ea26442697750d4e513b
changwon231 <tlsckdnjs@naver.com>
2019-07-10 16:01:29 +0900

    배열
    Array

---
+++ day05-Array/src/array/basic/ArrayAndFor.java
@@ -0,0 +1,33 @@
+package array.basic;
+
+/**
+ * 10 칸짜리 int 배열 numbers 를 선언, 생성(0 초기화) 한 후
+ * 
+ * 각 원소의 값을 0번 인덱스부터 9번 인덱스까지 
+ * 10, 20, 30,... 100 까지 저장 한다.
+ * 
+ * 배열에 저장된 원소의 합을 int summary; 변수에 누적하여 출력하시오.
+ * 
+ * @author Administrator
+ *
+ */
+public class ArrayAndFor {
+
+	public static void main(String[] args) {
+		// 1. 선언
+		int[] numbers;
+		int summery = 0;
+		
+		// 2. 초기화
+		numbers = new int[10];
+
+		// 3. 값 저장
+		for (int idx = 0; idx < numbers.length; idx++) {
+			numbers[idx] = (idx + 1) * 10;
+			summery += numbers[idx];
+			}System.out.printf("원소의 합 : %d%n", summery);
+		}
+		
+}
+
+
