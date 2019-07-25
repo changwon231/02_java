552a6250a6161798a083774e08e9dda54a629579
changwon231 <tlsckdnjs@naver.com>
2019-07-25 09:34:31 +0900

    인터페이스
    학교

---
+++ day13-Inheritance.school/src/school/Employee.java
@@ -0,0 +1,49 @@
+package school;
+/**
+ * 학교 시스템에서 교직원의 정보를 저장하는 클래스
+ * 
+ * 사람의 공통정보를 담는 클래스인 Person을 상속한다.
+ * -------------------------------------------------
+ * dept : String : 소속 부서 이름
+ * --------------------------------------------------
+ * @author Administrator
+ *
+ */
+public class Employee extends Person {
+	
+	// 1. 맴버변수 선언부
+	/** 직원의 소속부서 이름*/
+	private String dept;
+	
+	public Employee() {
+		
+	}
+	
+	public Employee(String dept) {
+		this.dept = dept;
+	}
+	/**
+	 * Employee 만의 필드 dept를 초기화 하는 생성자 
+	 *      dept : String : 소속 부서명
+	 */
+	
+	/**
+	 * Person 에서 물려받는 필드 (id name age)을
+	 * @param id
+	 * @param namem
+	 * @param agem
+	 */
+	public Employee(String id, String name, int age, String dept) {
+		super(id, name, age);
+		this.dept = dept;
+	}
+	
+	// 3. 메소드 선언부 
+	// (1) toStirng() 재정의
+	@Override
+	public String toString() {
+		String empStr = String.format(", 부서 : %s ", dept);
+		return String.format("직원 정보 [%s%s]", super.toString(), empStr);
+	}
+	
+}
---
+++ day13-Inheritance.school/src/school/SchoolTest.java
@@ -0,0 +1,53 @@
+package school;
+/**
+ * 학교 시스템을 테스트 하는 클래스이다.
+ * --------------------------------------
+ * Person  과 상속관계인 
+ * Student, Employee , Teacher 를 
+ * 테스트하는 클래스이다.
+ * @author Administrator
+ *
+ */
+public class SchoolTest {
+	
+	public static void main(String[] args) {
+		// 1. 선언
+		// Person 배열타입의 변수 persons를 선언
+
+		// 2. 초기화
+		// (1) persons 를 3 사이즈로 생성
+		Person[] persons = new Person[3];
+		
+		
+		// (2) persons 배열의 각각 0 ~ 2 인덱스에 
+		// [0] : Student : id = S01, name = 홍길동, age = 18, major = 도술
+		// [1] : Teacher : id = T01, name = 정약용, age = 25, dept = 실학
+		// [2] : Employee: id = E01, name = 허균,   age = 48, dept = 문학예술 지원
+		
+		persons[0] = new Student("S01", "홍길동", 18, "도술");
+		persons[1] = new Teacher("T01", "정약용", 25, "실학");
+		persons[2] = new Employee("E01", "허균 " , 48, "문학예술 지원");
+		
+		// 배열의 초기화 리스트 사용
+//		Person[] people = {new Student("S01", "홍길동", 18, "도술")
+//						  , new Teacher("T01", "정약용", 25, "실학")
+//						  , new Employee("E01", "허균", 48, "문학예술 지원")};
+		
+		
+		// 3. 사용 
+		// foreach 를 사용하여 persons 배열의 각 엘리먼트에 대하여 
+		// print() 메소드 호출
+		for(Person pe : persons) {
+			// 가상 메소드 호출(virtual method invocation)
+			// 상위클래스에 정의된 메소드를 호출해도
+			// 실제 인스턴스 타입의 하위 클래스에 재 정의된
+			// 메소드가 응답하는 것
+			// 가상메소드 호출이 되기 위해서는 
+			// 반드시 상속이 전제되어야 함
+			pe.print();
+		}
+		
+		
+	}
+
+}
